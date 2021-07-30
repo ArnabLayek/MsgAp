8c6cb9df1abc7bee005ccb38537d14fccd5d1400
Arnab211996 <arnab@tsdigisol.com>
2021-07-30 21:56:08 +0530

    Add all files

---
+++ src/main/java/com/msgAP/msgAP/MsgApApplication.java
@@ -0,0 +1,13 @@
+package com.msgAP.msgAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgApApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgApApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msgAP/msgAP/MsgApApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msgAP.msgAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgApApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
